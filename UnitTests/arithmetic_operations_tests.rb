819772fed92d59e5c34bf788ae41f0b3f92f466c
theWhyteRabbit <svidal@ualberta.ca>
2016-01-30 14:44:02 -0700

    added empty tests

--- UnitTests/arithmetic_operations_tests.rb
+++ UnitTests/arithmetic_operations_tests.rb
@@ -24,20 +24,82 @@ def testThing(arg1,arg2)
 class ArithmeticOperationsUnitTests < Test::Unit::TestCase
 
 # +
+def testAdditionNumericInt
+end
 
+def testAdditionNumericFloat
+end
+
+def testAdditionVectorInt
+end
+
+def testAdditionVectorFloat
+end
 
 # -
+def testSubtractionNumericInt
+end
+
+def testSubtractionNumericFloat
+end
+
+def testSubtractionVectorInt
+end
+
+def testSubtractionVectorFloat
+end
 
 
 # *
+def testMultiplicationNumericInt
+end
 
+def testMultiplicationNumericFloat
+end
+
+def testMultiplicationVectorInt
+end
+
+def testMultiplicationVectorFloat
+end
 
 # /
+def testDivisionNumericInt
+end
 
+def testDivisonNumericFloat
+end
+
+def testDivisionVectorInt
+end
+
+def testDivisionVectorFloat
+end
 
 
 # ** (eponetiation)
 
+def testExponentiationNumericInt
+end
+
+def testExponentiationNumericFloat
+end
+
+def testExponentiationVectorInt
+end
+
+def testExponentiationVectorFloat
+end
 
 
 end
+
+=begin
+we are using ints, floats and chars
+	- number
+	- vector
+	- matrix
+
+different tests for ints and floats.  what is a good tolerance for floats?
+
+=end
