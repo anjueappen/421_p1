76f31b68501209fd5eb24b66d0b19d5dc99c8ad4
theWhyteRabbit <svidal@ualberta.ca>
2016-01-30 14:30:11 -0700

    framework

--- UnitTests/arithmetic_operations_tests.rb
+++ UnitTests/arithmetic_operations_tests.rb
@@ -1,9 +1,42 @@
 require 'test/unit'
 require '../sparse_matrix.rb'
 
+=begin
+layout for tests
+
+def testThing(arg1,arg2)
+	
+	# comment(s) for clarity (brief description of what is being tested)
+
+	# pre conditions
+	
+	# method usage tests
+
+	#post conditions
+	
+	end #testThing
+
+	# invariants - for class?
+	
+=end
+
+
 class ArithmeticOperationsUnitTests < Test::Unit::TestCase
 
+# +
 
+
+# -
+
+
+# *
+
+
+# /
+
+
+
+# ** (eponetiation)
 
 
 
