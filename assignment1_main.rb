20c817f47a02b1d7e7d85bbd5cb4d661c487855d
mabuyo <mabuyo@ualberta.ca>
2016-02-09 16:10:14 -0700

    Added more to main file.

--- assignment1_main.rb
+++ assignment1_main.rb
@@ -9,6 +9,7 @@ require './tridiagonal_matrix.rb'
 
 # INITIALIZING SPARSE MATRICES
 # many different ways
+puts "INITIALIZING..."
 sm = SparseMatrix[[1,0,0,0], [0,2,0,0], [3,0,0,0], [0,0,0,4]]
 zero_sm = SparseMatrix.zero(3,3)
 diag_sm = SparseMatrix.diagonal(1,2,3,4)
@@ -21,14 +22,18 @@ compressed_sm = SparseMatrix.compressed_format({[0,0]=>1,[1,0]=>2,[1,1]=>3,[2,2]
 matrix = Matrix[[1,3,2,0], [1,2,1,1], [3,2,0,0], [0,0,0,4]]
 
 # SPARSITY ATTRIBUTES
+puts "SPARSITY ATTRIBUTES"
 puts sm.sparse? 	#yes
 puts sm.sparsity 	#0.25 
 puts sm.nonzero_count #4
 puts sm.nonzeros  #{[0,0]=>1, [1,1]=>2, [2,0]=>3, [3,3]=>4}
 puts sm.full  		#Matrix[[1,0,0,0], [0,2,0,0], [3,0,0,0], [0,0,0,4]]
+sm.putNonZero(4,0,1) #Matrix[[1,4,0,0], [0,2,0,0], [3,0,0,0], [0,0,0,4]]
+puts sm.full
 
 # ARITHMETIC OPERATIONS
 # full() is called for printing purposes
+puts "ARITHMETIC OPERATIONS"
 # Sparse matrices with sparse matrices
 puts (sm+diag_sm).full
 puts (sm-diag_sm).full
@@ -42,8 +47,15 @@ puts (sm*matrix).full
 puts (sm/matrix).full
 
 # Sparse matrices with a number
+puts (sm.increase_all_values_by(2)).full
+puts (sm.decrease_all_values_by(2)).full
+puts (sm*2).full
+puts (sm/2).full 
+puts (sm**2).full
 
-
+# MATRIX FUNCTIONS
+puts sm.transpose.full
+puts sm.trace
 
 # NOTE: Matrix functions that are not specified in this file can still be used, they will just be delegated to the Matrix class.
 
