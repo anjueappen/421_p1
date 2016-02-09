76b870ab06ffaca3bb19cd80c014d90e0390dd13
theWhyteRabbit <svidal@ualberta.ca>
2016-02-08 23:31:57 -0700

    test addition vector int done

--- UnitTests/arithmetic_operations_tests.rb
+++ UnitTests/arithmetic_operations_tests.rb
@@ -117,10 +117,9 @@ class ArithmeticOperationsUnitTests < Test::Unit::TestCase
 		begin
 			sparse_matrix+(value_to_add)
 		rescue Exception => e
-			if e.is_a? ErrOperationNotDefined
-				pass "Correct exception raised"
+				assert_true (e.is_a? ErrOperationNotDefined), "Incorrect exception thrown: #{e}"
 			else
-				fail "Incorrect excpetion raised"
+				fail 'No Exception thrown'
 			end
 		end
 		
@@ -153,10 +152,9 @@ class ArithmeticOperationsUnitTests < Test::Unit::TestCase
 		begin
 			sparse_matrix+(value_to_add)
 		rescue Exception => e
-			if e.is_a? ErrOperationNotDefined
-				pass "Correct exception raised"
+				assert_true (e.is_a? ErrOperationNotDefined), "Incorrect exception thrown: #{e}"
 			else
-				fail "Incorrect excpetion raised"
+				fail 'No Exception thrown'
 			end
 		end
 		
@@ -170,26 +168,37 @@ class ArithmeticOperationsUnitTests < Test::Unit::TestCase
 	
 	def test_addition_vector_int
 		#setup
-		 sparse_matrix1 = SparseMatrix[[4],[0],[0],[4]]  #4x1
-		 sparse_matrix2 = SparseMatrix[[1],[1],[0],[0]]  #4x1
-		 sparse_clone1 =  sparse_matrix1.clone()  # used to check that matrix used in operation was not changed
-		 sparse_clone2 =  sparse_matrix2.clone()  # used to check that matrix used in operation was not changed
+		sparse_matrix1 = SparseMatrix[[4],[0],[0],[4]]  #4x1
+		hash_sm1 = {[0,0]=>4, [3,0]=>4}
+		sparse_matrix2 = SparseMatrix[[1],[1],[0],[0]]  #4x1
+		hash_sm2 = {[0,0]=>1,[1,0]=>1}
+		sparse_clone1 =  sparse_matrix1.clone()  # used to check that matrix used in operation was not changed
+		sparse_clone2 =  sparse_matrix2.clone()  # used to check that matrix used in operation was not changed
+		 
+		hash_expected = {[0,0]=>5,[1,0]=>1,[3,0]=>4}
 		
 		#pre
 		assert_equal  sparse_matrix1.row_count,  sparse_matrix2.row_count, "Incompatible dimension (row) for matrix addition"
 		assert_equal  sparse_matrix1.column_count,  sparse_matrix2.column_count, "Incompatible dimension (column) for matrix addition"
+		assert hash_sm1.eql?(sparse_matrix1.values), "Hashes must be equal."
+		assert hash_sm2.eql?(sparse_matrix2.values), "Hashes must be equal."
+		
+		#invariant
+		checkMatrixAssertions(sparse_matrix1, sparse_clone1)
+		checkMatrixAssertions(sparse_matrix2, sparse_clone2)
 		
 		#data tests
-		 result_matrix =  sparse_matrix1+(@sparse_matrix2)
+		result_matrix =  sparse_matrix1+(sparse_matrix2)
 		assert_equal  result_matrix.full(), Matrix[[5],[1],[0],[4]], "Vector addition failed"
 		
 		#post
+		assert hash_sm1.eql?(sparse_matrix1.values), "Hashes must be equal."
+		assert hash_sm2.eql?(sparse_matrix2.values), "Hashes must be equal."
+		assert hash_expected.eql?(result_matrix.values), "Hashes must equal."
 		
 		#invariant
-		assert_equal  sparse_clone1,  sparse_matrix1, "Original matrix was altered"
-		assert !@sparse_matrix1.empty?
-		assert_equal  sparse_clone2,  sparse_matrix2, "Original matrix was altered"
-		assert !@sparse_matrix2.empty?
+		checkMatrixAssertions(sparse_matrix1, sparse_clone1)
+		checkMatrixAssertions(sparse_matrix2, sparse_clone2)
 		
 	end
 
